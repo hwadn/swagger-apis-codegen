656c6595a9cda3da4dbf1848f1eee15bc57c05c7
huadong.chen <huadong.chen@shopee.com>
2022-05-09 15:24:47 +0800

    feat: add handlebar templates register

--- src/utils/writeApis.ts
+++ src/utils/writeApis.ts
@@ -1,10 +1,8 @@
 import { OpenAPIV3 } from 'openapi-types'
-import Handlebars from 'handlebars'
-import { readFile } from 'fs/promises'
-import { resolve } from 'path'
-import apisTemplate from '@/templates/apis.hbs'
+import { apisRender } from '@/utils/registerHandlebarTemplates'
 
 export const writeApis = async (swaggerPaths: OpenAPIV3.Document['paths']) => {
   // TODO
-  console.log('apisTemplate:', apisTemplate)
+  const res = apisRender({ age: 12, name: 'ccchd' })
+  console.log('res:', res)
 }
