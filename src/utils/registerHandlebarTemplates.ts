c6d3c1c77585295b83919eab422a1e28aec26742
huadong.chen <huadong.chen@shopee.com>
2022-05-16 17:27:57 +0800

    feat: path parse

--- src/utils/registerHandlebarTemplates.ts
+++ src/utils/registerHandlebarTemplates.ts
@@ -1,8 +1,14 @@
 import Handlebars from 'handlebars'
 import apisTemplate from '@/templates/apis.hbs'
 import { ITagApis } from '@/interfaces/apis'
+import { OpenAPIV3 } from 'openapi-types/dist/index'
+import { swaggerPathToJs } from '@/utils/transform'
 
 export const apisRender = Handlebars.template<ITagApis>(apisTemplate)
 
 // helpers
 Handlebars.registerHelper('firstLowCase', (value: string) => value.replace(/^[A-Z]/, firstCh => firstCh.toLocaleLowerCase()))
+Handlebars.registerHelper('parsePath', (path: string, parameters: OpenAPIV3.PathItemObject['parameters']) => {
+  const pathParameters =  parameters?.filter(parameter => ('in' in parameter) && parameter.in === 'path')
+  return swaggerPathToJs(path, !!pathParameters && pathParameters?.length > 0)
+})
