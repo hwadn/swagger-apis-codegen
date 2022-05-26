145663be37af075df34377fb7da19218e59d34f8
huadong.chen <huadong.chen@shopee.com>
2022-05-26 18:37:15 +0800

    fix: eslint

--- src/index.ts
+++ src/index.ts
@@ -12,21 +12,21 @@ interface IGenerateConfig {
 }
 
 export const codeGen = async (config: IGenerateConfig) => {
-  console.log('start!')
-  const { input, output } = config
-  const swaggerObject = await readSwagger(input)
-  const { paths, components } = swaggerObject
-  // TODO check version
-  await writeModels(components?.schemas, output)
-  await writeApis(paths, output)
+	console.log('start!')
+	const { input, output } = config
+	const swaggerObject = await readSwagger(input)
+	const { paths, components } = swaggerObject
+	// TODO check version
+	await writeModels(components?.schemas, output)
+	await writeApis(paths, output)
 
-  console.log('done!')
+	console.log('done!')
 }
 
 // TODO remove
 const urlSrc = 'https://kube-test.devops.test.sz.shopee.io/api/v1/api-json'
 codeGen({
-  input: urlSrc,
-  type: 'server',
-  output: './test'
+	input: urlSrc,
+	type: 'server',
+	output: './test'
 })
--- src/utils/writeApis.ts
+++ src/utils/writeApis.ts
@@ -6,13 +6,13 @@ import { formatSwaggerPaths } from '@/utils/format'
 import { createDirectory } from '@/utils/file'
 
 export const writeApis = async (swaggerPaths: OpenAPIV3.Document['paths'], outputDir?: string) => {
-  const tagApis = formatSwaggerPaths(swaggerPaths)
-  const apisDirectory = resolve(__dirname, outputDir || './', 'apis')
-  // Absolute file path
-  await createDirectory(apisDirectory)
-  await Promise.all(tagApis.map(async (tagApis) => {
-    const apisCode = apisRender(tagApis)
-    const filePath = resolve(apisDirectory, `${tagApis.tag}.ts`)
-    await writeFile(filePath, apisCode)
-  }))
+	const tagApis = formatSwaggerPaths(swaggerPaths)
+	const apisDirectory = resolve(__dirname, outputDir || './', 'apis')
+	// Absolute file path
+	await createDirectory(apisDirectory)
+	await Promise.all(tagApis.map(async (tagApis) => {
+		const apisCode = apisRender(tagApis)
+		const filePath = resolve(apisDirectory, `${tagApis.tag}.ts`)
+		await writeFile(filePath, apisCode)
+	}))
 }
